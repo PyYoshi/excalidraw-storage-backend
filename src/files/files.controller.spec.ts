f4288d3e8bd6541c958d1bd9be636c20db9cfb47
PyYoshi <myoshi321go@gmail.com>
2025-02-21 20:08:17 +0900

    wip: add files.controller tests

--- src/files/files.controller.spec.ts
+++ src/files/files.controller.spec.ts
@@ -19,4 +19,26 @@ describe('FilesController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should call storageService.get', async () => {
+    const getSpy = jest
+      .spyOn(storageService, 'get')
+      .mockResolvedValue(Buffer.from('test'));
+    const res = {
+      pipe: jest.fn(),
+    } as any;
+    await controller.findOne('test', res);
+    expect(getSpy).toHaveBeenCalledWith('test', StorageNamespace.FILES);
+  });
+
+  it('should call storageService.set', async () => {
+    const setSpy = jest.spyOn(storageService, 'set').mockResolvedValue(true);
+    const result = await controller.create('test', Buffer.from('test'));
+    expect(setSpy).toHaveBeenCalledWith(
+      'test',
+      Buffer.from('test'),
+      StorageNamespace.FILES,
+    );
+    expect(result).toEqual({ id: 'test' });
+  });
 });
